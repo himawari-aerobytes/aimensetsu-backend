fbdd0857d0314bfc9bca14eefdbb80500a0db819
syuichi <n-syuichi@users.noreply.github.com>
2024-07-13 13:14:50 +0900

    threadID対応

--- rag_sample_app/admin.py
+++ rag_sample_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ChatHistory , Thread
 
 # Register your models here.
+admin.site.register(ChatHistory)
+admin.site.register(Thread)
--- rag_sample_app/models.py
+++ rag_sample_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.apps import AppConfig
+import uuid  # uuidモジュールをインポート
 
 class Document(models.Model):
     content = models.TextField()
@@ -10,8 +11,11 @@ class Document(models.Model):
 class RagSampleAppConfig(AppConfig):
     name = 'rag_sample_app'
 
+class Thread(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 class ChatHistory(models.Model):
-    thread_id = models.CharField(max_length=100,default='default_thread_id')  # 追加
+    thread_id = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='chats')
     # thread_idのデフォルト値を設定
 
     user_input = models.TextField()
@@ -20,3 +24,5 @@ class ChatHistory(models.Model):
 
     def __str__(self):
         return f"Thread {self.thread_id} - {self.user_input[:50]}"
+    
+
