1e9f108d9a548beb3512db5b40ca63519eaac7d4
himawari-aerobytes <himawari-aerobytes@users.noreply.github.com>
2024-07-14 21:08:20 +0900

    要約文の追加（送られてきたプロンプトのセット）

--- rag_sample_app/urls.py
+++ rag_sample_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DocumentList, OpenAIResponse, ChatHistoryListCreate
+from .views import DocumentList, OpenAIResponse, ChatHistoryListCreate,create_new_thread, ThreadSummary, AllThreads
 from .views import create_new_thread
 
 urlpatterns = [
@@ -7,5 +7,7 @@ urlpatterns = [
     path('openai/', OpenAIResponse.as_view(), name='openai-response'),
     path('chat-history/', ChatHistoryListCreate.as_view(), name='chat-history-list-create'),
     path('new-thread/', create_new_thread, name='new-thread'),
+    path('thread-summary/<str:thread_id>/', ThreadSummary.as_view(), name='thread_summary'),
+    path('all-threads/', AllThreads.as_view(), name='all_threads'),
 
 ]
