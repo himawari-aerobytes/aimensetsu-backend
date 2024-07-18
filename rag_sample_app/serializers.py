1534b42b53354f2aa8713e3cf4c5a50e5b83ebc6
himawari-aerobytes <himawari-aerobytes@users.noreply.github.com>
2024-07-18 19:23:56 +0900

    ログイン実装

--- rag_sample_app/serializers.py
+++ rag_sample_app/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Document
 from .models import ChatHistory
+from django.contrib.auth.models import User
 
 class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,13 @@ class ChatHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatHistory
         fields = ['id', 'thread_id', 'user_input', 'ai_response', 'timestamp']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- rag_sample_app/urls.py
+++ rag_sample_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import DocumentList, OpenAIResponse, ChatHistoryListCreate,create_new_thread, ThreadSummary, AllThreads
 from .views import create_new_thread
+from .views import RegisterUser
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+from .views import MyTokenObtainPairView
 
 urlpatterns = [
     path('documents/', DocumentList.as_view(), name='document-list'),
@@ -9,5 +14,9 @@ urlpatterns = [
     path('new-thread/', create_new_thread, name='new-thread'),
     path('thread-summary/<str:thread_id>/', ThreadSummary.as_view(), name='thread_summary'),
     path('all-threads/', AllThreads.as_view(), name='all_threads'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterUser.as_view(), name='register'),
+
 
 ]
