e087af7c27682f95627a6dbe56953b41758b4482
himawari-aerobytes <himawari-aerobytes@users.noreply.github.com>
2024-09-10 13:47:05 +0900

    ChatHistory__str__修正

--- rag_sample_app/models.py
+++ rag_sample_app/models.py
@@ -1,8 +1,9 @@
-from django.db import models
-from django.apps import AppConfig
 import uuid  # uuidモジュールをインポート
-from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractBaseUser
+
+from django.apps import AppConfig
+from django.contrib.auth.models import AbstractBaseUser, User
+from django.db import models
+
 
 class Document(models.Model):
     content = models.TextField()
@@ -10,27 +11,36 @@ class Document(models.Model):
     def __str__(self):
         return self.content[:50]
 
+
 class RagSampleAppConfig(AppConfig):
-    name = 'rag_sample_app'
+    name = "rag_sample_app"
+
 
 class Thread(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     summary = models.TextField(blank=True, null=True)  # 要約フィールドを追加
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)  # 作成者フィールドを追加
-    first_message = models.TextField(blank=True, null=True)  # 初回メッセージフィールドを追加
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )  # 作成者フィールドを追加
+    first_message = models.TextField(
+        blank=True, null=True
+    )  # 初回メッセージフィールドを追加
+
+
 class ChatHistory(models.Model):
-    thread_id = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='chats')
+    thread_id = models.ForeignKey(
+        Thread, on_delete=models.CASCADE, related_name="chats"
+    )
     # thread_idのデフォルト値を設定
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     sender = models.TextField()
 
     def __str__(self):
-        return f"Thread {self.thread_id} - {self.message[:50]}"
-    
+        return f"Thread {self.thread_id.id} - {self.message[:50]}"
+
+
 class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
-    USERNAME_FIELD = 'email'
-    
-
+    USERNAME_FIELD = "email"
