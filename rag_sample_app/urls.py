2e5b191eb3efbecd72c040fd7b6083dffe6d9228
syuichi <n-syuichi@users.noreply.github.com>
2024-09-11 16:44:13 +0900

    不要なurlを削除

--- rag_sample_app/urls.py
+++ rag_sample_app/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import (
     AllThreads,
-    ChatHistoryListCreate,
+    ChatHistoryList,
     DeleteThread,
     DocumentList,
-    LogoutView,
-    MyTokenObtainPairView,
     OpenAIResponse,
-    RegisterUser,
     ThreadSummary,
     create_new_thread,
     get_first_message,
@@ -20,8 +16,8 @@ urlpatterns = [
     path("openai/", OpenAIResponse.as_view(), name="openai-response"),
     path(
         "chat-history/",
-        ChatHistoryListCreate.as_view(),
-        name="chat-history-list-create",
+        ChatHistoryList.as_view(),
+        name="chat-history-list",
     ),
     path("new-thread/", create_new_thread, name="new-thread"),
     path(
@@ -29,15 +25,11 @@ urlpatterns = [
         ThreadSummary.as_view(),
         name="thread-summary",
     ),
-    path("all-threads/", AllThreads.as_view(), name="all_threads"),
-    path("token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("register/", RegisterUser.as_view(), name="register"),
+    path("all-threads/", AllThreads.as_view(), name="all-threads"),
     path(
         "delete-thread/<uuid:thread_id>/", DeleteThread.as_view(), name="delete-thread"
     ),
     path(
         "first-message/<uuid:thread_id>/", get_first_message, name="get-first-message"
     ),
-    path("logout/", LogoutView.as_view(), name="logout"),
 ]
